e0f96d618dc73b15747a4249a63b925ab3608d0a
sabbir hossain <sabbirhossain.py@gmail.com>
2025-01-17 19:26:57 +0600

    working on metatag

--- app/animal/[name]/page.tsx
+++ app/animal/[name]/page.tsx
@@ -9,8 +9,22 @@ export async function generateMetadata({ params }: any): Promise<Metadata> {
   const { name } = await params;
 
   return {
-    title: `${name} - Animly`,
-    description: `Learn more about ${name}, its characteristics, habitat, and more.`
+    title: `${name} - Animly | Learn About ${name}`,
+    description: `Discover all about ${name}, including its characteristics, habitat, and behaviors. Dive into the world of wildlife with Animly.`,
+    keywords: `${name}, ${name} characteristics, ${name} habitat, animal encyclopedia, wildlife research`,
+    openGraph: {
+      title: `${name} - Animly`,
+      description: `Explore detailed information about ${name}. Learn about its habitat, behaviors, and unique traits.`,
+      images: [
+        {
+          // url: `https://animly.vercel.app/animlydemo/${name}`,
+          url: `https://animly.vercel.app/animlydemo.png`,
+          width: 1200,
+          height: 630,
+          alt: `Detailed information about ${name}`,
+        },
+      ],
+    },
   }
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,22 @@ import { PawPrint } from "lucide-react";
 
 
 export const metadata: Metadata = {
-  title: "Animly",
+  title: "Animly - Simple Animal Encyclopedia",
   description: "Explore Animly, your modern animal encyclopedia designed for nature enthusiasts and wildlife lovers. Discover fascinating facts, habitats, and behaviors about animals from around the globe. Whether you're researching for school, planning an adventure, or simply curious about the animal kingdom, Animly offers detailed, engaging, and easy-to-read content. Start your journey into the wild today with Animly!",
+  keywords: "animly.vercel.app, animly, vercel animals, animal facts, wildlife, habitats, animal encyclopedia, nature, wildlife research, animal behaviors",
+  openGraph: {
+    title: "Animly - Modern Animal Encyclopedia",
+    description: "Explore fascinating animal facts and behaviors with Animly, the ultimate wildlife resource.",
+    url: "https://animly.vercel.app",
+    images: [
+      {
+        url: "https://animly.vercel.app/animlydemo.png",
+        width: 1200,
+        height: 630,
+        alt: "Animly - Modern Animal Encyclopedia",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -23,12 +37,12 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="min-h-screen flex flex-col">
             <header className="bg-white dark:bg-gray-800 shadow-md">
-              <div className="container mx-auto px-6 py-2 md:px-10 md:py-4 flex justify-between items-center">
+              <nav className="container mx-auto px-6 py-2 md:px-10 md:py-4 flex justify-between items-center">
                 <Link href='/' className="flex flex-col items-center justify-center">
                   <PawPrint className="h-6 w-6 md:h-9 md:w-9" /> Animly
                 </Link>
                 <ThemeToggle />
-              </div>
+              </nav>
             </header>
             <main className="flex-grow container mx-auto px-4 py-8">
               {children}
